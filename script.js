1b9c60af84ef7643bf953babeb3eeaa8fc88a2e0
Mahmoud <mahmoud.w.diab@gmail.com>
2021-03-16 21:22:03 +0200

    added helper function to stay dry

--- script.js
+++ script.js
@@ -931,19 +931,30 @@ const apiKey = ACCESS_KEY;
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper function to set Attributes on DOM elements
+const setAttributes = (element, attributes) => {
+  for (let key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
 // Create elements for links and photos, Add it to the DOM
 const displayPhotos = () => {
   // Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // Create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
