bc5c6bbe6d659a6428a06f63d0737420f306ae61
Mahmoud <mahmoud.w.diab@gmail.com>
2021-03-16 16:58:48 +0200

    fetching from api works, hid the api key

---
+++ script.js
@@ -0,0 +1,20 @@
+// Unsplash API
+const count = 10;
+const apiKey = config.ACCESS_KEY;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+
+const getPhotos = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    // Catch error here
+    console.log;
+  }
+};
+
+// On load
+// getPhotos();
